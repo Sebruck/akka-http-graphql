4cb45e00d2c8c4752796673ad251c0690ba477a5
Scala Steward <me@scala-steward.org>
2020-05-10 17:37:33 +0200

    Update scalatest to 3.1.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   val akkaHttpVersion       = "10.1.11"
   val akkaVersion           = "2.6.4"
-  val scalaTestVersion      = "3.1.1"
+  val scalaTestVersion      = "3.1.2"
   val akkaHttpCirceVersion  = "1.32.0"
   val circeVersion          = "0.13.0"
   val circeOpticsVersion    = "0.13.0"
