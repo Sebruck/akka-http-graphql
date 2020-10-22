190195e440d9010eb7121574cab8fce1812035f4
Scala Steward <me@scala-steward.org>
2020-10-22 15:23:19 +0200

    Update sangria-circe to 1.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   val circeOpticsVersion    = "0.13.0"
   val sangriaVersion        = "2.0.0-RC1"
   val sangriaSlowlogVersion = "2.0.0-M1"
-  val sangriaCirceVersion   = "1.3.0"
+  val sangriaCirceVersion   = "1.3.1"
 
   lazy val scalaTest = Seq("org.scalatest" %% "scalatest" % scalaTestVersion % Test)
 
